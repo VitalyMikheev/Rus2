a27283e72f2b49914f39cd75e15adcb0ff2a825a
Vitaly Mikheev <vitaly.mikheev@hotmail.com>
2024-09-21 20:01:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $(".slider-one").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1100,
+    centerMode: true,
+  });
+  $(".slider-two").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    centerMode: true,
+  });
+});
